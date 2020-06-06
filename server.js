414cfed9ff418f654339d4ba0d46dcc6d017fe6f
Bibek Shah Shankhar <shahshankharbibek@gmail.com>
2020-06-06 22:41:54 +0545

    Update server.js

--- server.js
+++ server.js
@@ -1,41 +1,34 @@
 // Setup empty JS object to act as endpoint for all routes
-projectData = {};
+let projectData = {};
 
 // Require Express to run server and routes
 const Express = require('express');
-const bodyParser = require('body-parser');
-const port = 8080;
-
 // Start up an instance of app
 const app = Express();
-
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
+const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-const Cors = require('cors');
-app.use(Cors());
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(Express.static('website'));
 
-
 // Setup Server
-const server = app.listen(port,listening);
-function listening() {
-    console.log('Server is running');
-    console.log(`running on localhost: ${port}`);
-};
-app.get('/data',(req,res)=> {
+const port = 8080;
+const server = app.listen(port, ()=>{console.log(`running on localhost: ${port}`)})
+
+app.get('/data', (req, res) => {
     console.log('GET Request Received');
     res.send(projectData);
 });
 app.post('/', (req, res) => {
     projectData.date = req.body.date;
-    projectData.temperature = req.body.temp;
+    projectData.temperature = req.body.main.temp;
     projectData.feelings = req.body.feelings;
-    console.log('POST request received');
+    console.log('POST Request Received');
     res.end();
 });
